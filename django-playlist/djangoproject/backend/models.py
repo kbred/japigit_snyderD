54dd9a719b3358b2b9a3d8a691b17bcf86a1892d
Tapdec <44556212+Tapdec@users.noreply.github.com>
2019-11-13 17:58:51 -0800

    new project
    new change

---
+++ django-playlist/djangoproject/backend/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+
+class TransferredSubscription(models.Model):
+    transferfrom = models.CharField(max_length=50)
+    transferto = models.CharField(max_length=50)
+    requestdate = models.DateTimeField()
+    transferdate = models.DateTimeField()
+    subscriberID = models.ForeignKey('Subscriber', on_delete=models.CASCADE)
+
+class Subscriber(models.Model):
+    username = models.ForeignKey('UserInfo', on_delete=models.CASCADE)
+    subscriptiontypecode = models.ForeignKey('SubscriptionType', on_delete=models.CASCADE)
+    servicecode = models.ForeignKey('Service', on_delete=models.CASCADE)
+    requestdate = models.DateTimeField()
+    startdate = models.DateTimeField()
+    enddate = models.DateTimeField()
+    motifofcancellation = models.TextField()
+
+class Officer(models.Model):
+    officecode = models.ForeignKey('Office', on_delete=models.CASCADE)
+    subscriberID = models.ForeignKey('Subscriber', on_delete=models.CASCADE)
+    startdate = models.DateTimeField()
+    enddate = models.DateTimeField()
+
+class OrganizationMember(models.Model):
+    organizationcode = models.ForeignKey('Organization', on_delete=models.CASCADE)
+    subscriberID = models.ForeignKey('Subscriber', on_delete=models.CASCADE)
+    startdate = models.DateTimeField()
+    enddate = models.DateTimeField()
+    nativecountry = models.CharField(max_length=20)
+    citizenship = models.CharField(max_length=20)
+    isdelegate = models.CharField(max_length=20)
+
+class UserInfo(models.Model):
+    username = models.CharField(max_length=20)
+    firstname = models.CharField(max_length=20)
+    middlename = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    email = models.EmailField()
+    address1 = models.CharField(max_length=50)
+    address2 = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.IntegerField(null=True)
+    employername = models.CharField(max_length=20)
+
+class SubscriptionType(models.Model):
+    subscriptiontypecode = models.IntegerField()
+    subscriptiontypename = models.CharField(max_length=50)
+
+class Service(models.Model):
+    servicecode = models.IntegerField()
+    servicename = models.CharField(max_length=20)
+    description = models.TextField()
+    premium = models.BooleanField()
+    allocation = models.CharField(max_length=50)
+
+class Office(models.Model):
+    officecode = models.IntegerField()
+    officename = models.CharField(max_length=50)
+    attribution = models.TextField()
+
+class Organization(models.Model):
+    organizationcode = models.IntegerField()
+    organizationname = models.CharField(max_length=50)
+    description = models.TextField()
+    datejoined = models.DateTimeField()
+    address1 = models.CharField(max_length=50)
+    address2 = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.IntegerField(null=True)
+    phonenumber = models.IntegerField(null=True)
--- django-playlist/djangoproject/djangoproject/settings.py
+++ django-playlist/djangoproject/djangoproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'articles',
     'accounts',
     'success',
+    'backend',
 ]
 
 MIDDLEWARE = [
